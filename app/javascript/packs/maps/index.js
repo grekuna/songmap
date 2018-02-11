209465514b5e9386bc381adf263d128c3db63333
grekuna <gregor.martius@gmail.com>
2018-02-11 18:43:10 +0100

    add webpacker, working gmap

---
+++ app/javascript/packs/maps/index.js
@@ -0,0 +1,9 @@
+document.addEventListener("turbolinks:load", function() {
+    
+    var map = new GMaps({
+    div: '#map',
+    lat: 38.5816,
+    lng: -121.4944
+    });
+
+});
---
+++ db/migrate/20180211125039_create_songs.rb
@@ -0,0 +1,11 @@
+class CreateSongs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.integer :artist
+      t.string :album
+
+      t.timestamps
+    end
+  end
+end
