0068e61802f31776d3b0f252b2fde24fe99f21b9
grekuna <gregor.martius@gmail.com>
2018-02-13 09:11:05 +0100

    updated new form

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,13 +2,17 @@ class Song < ApplicationRecord
     geocoded_by :address 
     after_validation :geocode, if: :address_changed?
 
+    validates :street, :city, :country, :name, :album, :artist, presence: true
+
     def address
-        [street, city, zip, state, country].compact.join(", ")
+      [street, city, zip, state, country].compact.join(", ")
     end
 
 
   def address_changed?
     street_changed? || city_changed? || zip_changed? || state_changed? || country_changed?
   end
+
+  
 
 end
