4fb78faa6f18719fcab94cab56db80415887f7e5
grekuna <gregor.martius@gmail.com>
2018-02-17 13:17:38 +0100

    delete unneeded routes and controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :songs
+  get '/songs', to: 'songs#index'
+  post '/songs', to: 'songs#create'
+  root to: "songs#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
